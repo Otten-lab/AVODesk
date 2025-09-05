146b593b6ea3709ac6faeeef4df81036b2d90fec
Otten-lab <otten.lab.ai@gmail.com>
2025-09-05 22:49:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: true, // Разрешает запросы с любых доменов
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
